d1d9b0543027a08271babb471fabbcd509a66e2f
hplar05 <ralph.saladino05@gmail.com>
2025-05-27 17:47:38 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      Hello Arvin
+      <span className="text-xl font-bold">
+        Manhwa<span className="text-orange-500">RaJ</span>
+      </span>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, User } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
@@ -11,27 +11,29 @@ export function Navbar() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <header className="h-20 flex items-center justify-center sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
-      <div className="container flex h-16 items-center justify-between">
+    <header className="h-30 flex items-center justify-center sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
+      <div className="container flex h-16 items-center justify-between xl:mx-[30vh] lg:mx-[15vh] md:mx-[6vh] max-md:mx-6 ">
         {/* Logo */}
-        <div className="flex items-center space-x-2 max-md:ml-3">
-          <Image
-            className="flex  max-sm:hidden "
-            src="/images/logo.png"
-            alt=""
-            width={70}
-            height={70}
-          />
-          <span className="text-xl font-bold">
-            Manhwa<span className="text-orange-500">RaJ</span>
-          </span>
+        <div className="flex items-center space-x-2 max-md:ml-3 ">
+          <a href="/" className="flex items-center justify-center space-x-2">
+            <Image
+              className=""
+              src="/images/logo.png"
+              alt=""
+              width={80}
+              height={80}
+            />
+            <span className="text-xl font-bold">
+              Manhwa<span className="text-orange-500">RaJ</span>
+            </span>
+          </a>
         </div>
 
         {/* Desktop Navigation */}
-        <div className="flex items-center justify-center gap-10">
-          <div className="text-md font-medium leading-none">Home</div>
-          <div className="text-md font-medium leading-none">Latest</div>
-          <div className="text-md font-medium leading-none">All Manhwa</div>
+        <div className="flex items-center justify-center gap-10 max-md:hidden xl:pr-[7rem] lg:pr-[3rem] md:pr-[2rem] ">
+          <div className="text-xl font-medium leading-none">Home</div>
+          <div className="text-xl font-medium leading-none">Latest</div>
+          <div className="text-xl font-medium leading-none">All Manhwa</div>
         </div>
 
         {/* Right Side Actions */}
@@ -41,44 +43,19 @@ export function Navbar() {
             variant="ghost"
             size="icon"
             onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-            className="hidden md:inline-flex"
+            className=" md:inline-flex"
           >
             <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
           </Button>
           {/* User Menu */}
-          {/* <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="ghost"
-                size="icon"
-                className="hidden md:inline-flex"
-              >
-                <User className="h-4 w-4" />
-                <span className="sr-only">User menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <BookOpen className="mr-2 h-4 w-4" />
-                Reading List
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <TrendingUp className="mr-2 h-4 w-4" />
-                Favorites
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Clock className="mr-2 h-4 w-4" />
-                History
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Sign out</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu> */}
+
+          <Button variant="ghost" size="icon" className="hidden md:inline-flex">
+            <User className="h-4 w-4" />
+            <span className="sr-only">User menu</span>
+          </Button>
+
           {/* Mobile Menu */}
           {/* <Sheet>
             <SheetTrigger asChild>
